41bc10c804fa75e22836f6a29e691c00d0bb0806
lucasalonia <salonialuca97@gmail.com>
2024-08-22 19:58:25 -0300

    Menu main

--- src/tp2grupo8_github/Tp2Grupo8_github.java
+++ src/tp2grupo8_github/Tp2Grupo8_github.java
@@ -40,8 +40,8 @@ public class Tp2Grupo8_github {
         
         System.out.println("MENU");
         System.out.println("1. Cuenta pasos.");
-        System.out.println("2. Setear hora");
-        System.out.println("3. Setear dias");
+        System.out.println("2. Consultar hora");
+        System.out.println("3. Consultar dia");
         System.out.println("4. Incrementar Dia");
         System.out.println("5. Incrementar Hora");
         System.out.println("6. Decir hora");        
@@ -56,36 +56,47 @@ public class Tp2Grupo8_github {
             int opcion = leer.nextInt();
             switch (opcion) {
                 case 1:
+                    System.out.println("¿Que distancia recorrio");
+                    int distancia = leer.nextInt();
+                    reloj.cuentaPasos(distancia);
                     break;
                  case 2:
-                     System.out.println("¿Que hora es?");
-                     String hora=leer.nextLine();
-                    reloj.setHora(hora);
-                    break;
+                     System.out.println("La hora es: ");
+                     reloj.getHora();
+                     break;
                 case 3:
-                    System.out.println("¿Que dias es?");
-                     String dia=leer.nextLine();
-                    reloj.setHora(dia);
+                    System.out.println("El dia es: ");
+                     reloj.getDia();
                     break;
                 case 4:
+                    reloj.incrementarDia();
+                    System.out.println("El dia avanza a: "+reloj.getDia());
                     break;
                 case 5:
+                    reloj.incrementarHora();
+                    System.out.println("La hora avanza a: "+reloj.getHora());
                     break;
                 case 6:
+                    persona.decirHora(reloj);
                     break;
                 case 7:
+                    persona.comer();
                     break;
                 case 8:
+                    System.out.println("Pantalla");
                     break;
                 case 9:
+                    persona.hablar();
                     break;
                 case 10:
+                    System.out.println("La frecuencia es: "+reloj.frecuenciaAleatoria());
                     break;
                case 11:
                    salir=false;
                     break;      
                 default:
-                    throw new AssertionError();
+                    System.out.println("Opcion incorrecta");
+                    break;
             }
         }
         
