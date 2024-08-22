f0d08c7706a8c62beb9144bf70214148bb0e2b71
nelsitonn <nelsonyaell@gmail.com>
2024-08-22 18:18:03 -0300

    creacion de clase Persona
    cree la clase persona

---
+++ src/tp2grupo8_github/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2grupo8_github;
+
+/**
+ *
+ * @author nelso
+ */
+public class Persona {
+    
+}
