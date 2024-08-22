4e2a78befac42a09af8bf2198a887d6d31fe1320
nelsitonn <nelsonyaell@gmail.com>
2024-08-22 18:33:18 -0300

    Persona

--- src/tp2grupo8_github/Persona.java
+++ src/tp2grupo8_github/Persona.java
@@ -10,4 +10,47 @@ package tp2grupo8_github;
  */
 public class Persona {
     
+     private String nombre, apellido;
+    private int edad;
+    private double altura;
+    //private Reloj unReloj = null;
+
+    public Persona(String nombre, String apellido, int edad, double altura) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.altura = altura;
+        //this.unReloj = unReloj;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public  void comer() {
+            System.out.println("comiendo......");
+}
+    public void hablar() {
+            System.out.println("Hola como te encuentras ");
+    }
+//        public void decirHora() {
+//        if (unReloj != null) {
+//            System.out.println("La hora es: " + unReloj.getHora());
+//        } else {
+//            System.out.println("No tengo un reloj asociado.");
+//        }
+//    }
+    
+    
+    
 }
